0ddcd396a4c1681d36e209bcb0bc0d2196c7f07a
Madison Moulder <madmogalaxy89@att.net>
2024-05-28 13:07:14 -0400

    initial commit

--- index.js
+++ index.js
@@ -7,9 +7,16 @@ const app = express();
 
 const PORT = 8080;
 
+const baseQuery = "/api/";
+
+
+
 // GET - / - returns homepage
 app.get('/', (req, res) => {
     // serve up the public folder as static index.html file
+    res.json({
+        success:true
+    })
 
 });
 
@@ -18,17 +25,22 @@ app.get('/api', (req, res) => {
     res.send('Hello World!');
 });
 
+
 // get all pets from the database
 app.get('/api/v1/pets', (req, res) => {
     // send the pets array as a response
-
+    res.send(pets);
 });
 
 // get pet by owner with query string
+
+
 app.get('/api/v1/pets/owner', (req, res) => {
     // get the owner from the request
+      res.send(pets.owner);
+       
 
-
+    
     // find the pet in the pets array
     const pet = pets.find(pet => pet.owner === owner);
 
@@ -39,10 +51,24 @@ app.get('/api/v1/pets/owner', (req, res) => {
 // get pet by name
 app.get('/api/v1/pets/:name', (req, res) => {
     // get the name from the request
+   const {id}=req.params;
+
+   const baseQuery = pets.find(i=>i.id===Number(id));
+   if(pets){
+    res.send(pets)
+    }else{
+        res.send("Student Not Found")
+    }         
 
 
     // find the pet in the pets array
     const pet = pets.find(pet => pet.name === name);
+    if(pet){
+        res.send(pet);
+
+    }else{
+        res.send("Pet Not Found")
+    }
 
     // send the pet as a response
 
